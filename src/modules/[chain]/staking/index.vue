c7cf2f28c35eb4e4d42a4d3867eaa031b71c1fa2
Your Name <hamasshakoor123@gmail.com>
2025-10-18 13:00:37 +0500

    Hamas2.0

--- src/modules/[chain]/staking/index.vue
+++ src/modules/[chain]/staking/index.vue
@@ -409,11 +409,7 @@ loadAvatars();
                                                         {{ v.description?.moniker }}
                                                     </RouterLink>
                                                 </span>
-<<<<<<< HEAD
                                                 <span class="text-[10px]">{{ v.operator_address || v.description?.identity || '-' }}</span>
-=======
-                                                <span class="text-xs">{{v.operator_address || v.description?.identity || '-'}}</span>
->>>>>>> origin/Hamas
                                             </div>
                                         </div>
                                     </td>
@@ -425,15 +421,9 @@ loadAvatars();
                                     <td class="text-right">
                                         <div class="flex flex-col">
                                             <h6 class="text-sm font-weight-medium whitespace-nowrap ">
-<<<<<<< HEAD
                                                 {{ format.formatToken({amount: parseInt(v.tokens).toString(), denom: staking.params.bond_denom,}, true, '0,0') }}
                                             </h6>
-                                            <span class="text-xs">{{ format.calculatePercent(v.delegator_shares, staking.totalPower) }}</span>
-=======
-                                                {{format.formatToken({amount: parseInt(v.tokens).toString(), denom: staking.params.bond_denom,}, true, '0,0')}}
-                                            </h6>
                                             <span class="text-xs">{{format.calculatePercent(v.delegator_shares, staking.totalPower)}}</span>
->>>>>>> origin/Hamas
                                         </div>
                                     </td>
                                     <!-- 👉 24h Changes -->
@@ -444,16 +434,10 @@ loadAvatars();
                                     <td class="text-xs text-right">{{ selfBonded?.balance ? $format.formatToken(selfBonded.balance) : '0' }}</td>
 
                                     <!-- 👉 commission -->
-<<<<<<< HEAD
                                     <td class="text-right text-xs">{{ format.formatCommissionRate(v.commission?.commission_rates?.rate) }}</td>
 
                                     <!-- 👉 Max commission -->
                                     <td class="text-right text-xs">{{ format.formatCommissionRate(v.commission?.commission_rates?.max_rate) }}</td>
-=======
-                                    <td class="text-right text-xs">
-                                        {{format.formatCommissionRate(v.commission?.commission_rates?.rate)}}
-                                    </td>
->>>>>>> origin/Hamas
                             </tr>
                         </tbody>
                     </table>
